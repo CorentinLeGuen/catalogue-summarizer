1a700034d8859d937a331a3d7839ad02a8a34ef5
CorentinLeGuen <leguen.corentin@protonmail.com>
2025-04-30 15:00:00 -0400

    Add linter fixes

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+"""
+main.py - catalogue summarizer is used to call an OpenAI agent who is going to summarize a text.
+"""
 import os
 
 from openai import OpenAI
@@ -12,10 +15,18 @@ client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 app = FastAPI()
 
 class LongText(BaseModel):
+    """
+    Reprénsente le texte en entrée
+    """
     text: str
 
 @app.post("/summarize")
 def summarize_text(data: LongText):
+    """
+    Call an OpenAI agent to summarize a text.
+    :param data: The text to be summarized.
+    :return: The summarized text.
+    """
     response = client.responses.create(
         model="gpt-4o",
         instructions="Tu es un assistant qui résume les textes.",
